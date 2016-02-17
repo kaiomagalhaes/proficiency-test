dc2a24b1db999c1b26709dab4e444e99de74de01
me@kaiomagalhaes.com <me@kaiomagalhaes.com>
2016-02-17 17:40:57 -0500

    add tests to grade

--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Grade, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'grade is valid' do
+    let(:grade) do
+      build(:grade)
+    end
+
+    it 'has a value' do
+      expect(grade).to be_valid
+    end
+  end
 end
