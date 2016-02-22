ceead7e214e707095a299a9548de8fea08a365b0
Thiago Durante Pires <thdurante@gmail.com>
2016-02-22 12:04:46 -0300

    Lint/EndAlignment fixed for flash_helper.rb

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -2,12 +2,12 @@ module FlashHelper
   def flash_messages
     flash.collect do |key, msg|
       alert_class = case(key)
-      when :notice
-        "success"
-      when :alert
-        "error"
-      else
-        "info"
+        when :notice
+          "success"
+        when :alert
+          "error"
+        else
+          "info"
       end
 
       content_tag(:div, msg, :id => key, :class => "alert #{alert_class} center flash-message")
