cb55c299114b0811cdc2d8f40a65eb8271d541e8
Ricardo Henrique <ricardohenrique996@gmail.com>
2016-02-23 11:20:12 -0300

    Editado o arquivo classroom.rb para que ele não possua strings com aspas duplas, espaços em branco extras, e guard clause.

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,22 +1,20 @@
+# Serve as model for Classroom entity
 class Classroom < ActiveRecord::Base
   validates :student, presence: true
   validates :course, presence: true
   validate :cannot_duplicate_student_and_course
-
 
   belongs_to :student
   belongs_to :course
 
   def cannot_duplicate_student_and_course
     old_classroom = Classroom.find_by(student: student, course: course)
-    if old_classroom
-      errors.add(:student, "Student already on this course!")
+    return unless old_classroom
+    errors.add(:student, 'Student already on this course!')
     end
   end
 
-  before_save(:on => :create) do
+  before_save(on: :create) do
     self.entry_at = Time.now.utc
   end
-
-
 end
