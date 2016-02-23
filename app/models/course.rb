c7b2c57e1f386040d4cd5143323647150440eaa1
Thiago Durante Pires <thdurante@gmail.com>
2016-02-23 13:06:39 -0300

    Solved some low priority warnings for course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,16 +1,17 @@
+# Serve as model for Course entity
 class Course < ActiveRecord::Base
   include Validate
 
-  validates :description, presence: true, length: {maximum: 45}
+  validates :description, presence: true, length: { maximum: 45 }
   has_many :students, through: :classrooms
 
   def destroy
     class_room = Classroom.find_by(course: self)
     if class_room
-      errors.add(:course, "Course alread in a classroom! Delete the classroom before!")
+      errors.add(:course,
+                 'Course alread in a classroom! Delete the classroom before!')
       return false
     end
     super
   end
-
 end
