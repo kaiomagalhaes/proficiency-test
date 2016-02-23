a7616d6a6bc200ac102db93009e5ae5b59bedc3b
Ricardo Henrique <ricardohenrique996@gmail.com>
2016-02-23 09:33:03 -0300

    Editado o flash_helper.rb para adequar-se aos padrões de qualidade do Rubocop (Mudança de tipos de string, mudança de padrão de key/value, mensagem de descrição da classe/modulo)

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,17 +1,19 @@
+# Serve as helper for flash messages (Messages on page that display a 
+# state of an action)
 module FlashHelper
   def flash_messages
     flash.collect do |key, msg|
       alert_class =
-        case(key)
-          when :notice
-            "success"
-          when :alert
-            "error"
-          else
-            "info"
+        case (key)
+	    when :notice
+		  'success'
+        when :alert
+          'error'
+        else
+          'info'
         end
 
-      content_tag(:div, msg, :id => key, :class => "alert #{alert_class} center flash-message")
+      content_tag(:div, msg, id: key, class: "alert #{alert_class} center flash-message")
     end.join.html_safe
   end
 end
