49268ffbc197496c4e385ccbfb67b01cd351d2fb
Ricardo Henrique <ricardohenrique996@gmail.com>
2016-02-23 09:58:48 -0300

    Modificado o alias_method para alias e modificado a forma de passagem dos métodos que terão um alias.

--- app/helpers/translation_helper.rb
+++ app/helpers/translation_helper.rb
@@ -9,7 +9,7 @@ module TranslationHelper
   def translate_model_name(model, count = 1)
     model.model_name.human(count: count)
   end
-  alias_method :tm, :translate_model_name
+  alias :tm :translate_model_name
 
   # Translate model name pluralized
   #
@@ -18,7 +18,7 @@ module TranslationHelper
   def translate_model_name_pluralized(model)
     translate_model_name(model, count = 2)
   end
-  alias_method :tmp, :translate_model_name_pluralized
+  alias :tmp :translate_model_name_pluralized
 
   # Translate a model attribute from config/locales/models-*.yml for more details.
   #
@@ -29,5 +29,5 @@ module TranslationHelper
   def translate_model_attribute(model, attribute, count = 1)
     model.human_attribute_name(attribute, count: count)
   end
-  alias_method :ta, :translate_model_attribute
+  alias :ta :translate_model_attribute
 end
