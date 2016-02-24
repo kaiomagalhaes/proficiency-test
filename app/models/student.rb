177c93f300e167da5be221ab0c8144ea755bd97c
Gabriel Rascovit <gabriel_louzada@hotmail.com>
2016-02-24 01:08:38 -0300

    Adicionando espaços dentro de chaves para melhor legibilidade; alterando para utilizaçao de single quoted string.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,13 +1,14 @@
+# Serve as model for Student entity
 class Student < ActiveRecord::Base
   include Validate
 
-  validates :register_number, presence: true, uniqueness: true, length: {maximum: 45}
+  validates :register_number, presence: true, uniqueness: true, length: { maximum: 45 }
   has_many :courses, through: :classrooms
 
   def destroy
     class_room = Classroom.find_by(student: self)
     if class_room
-      errors.add(:student, "Student alread in a classroom! Delete the classroom before!")
+      errors.add(:student, 'Student alread in a classroom! Delete the classroom before!')
       return false
     end
     super
